34149c0188bc736edf900797becc80ba15ac5c5b
David Tierney <d.r.tierney@hotmail.co.uk>
2020-07-05 11:37:49 +0100

    Stage #4 completed
    Stage #4 X-files
    Enable your search engine to search for data stored in the files.

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,14 +1,26 @@
 package search;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
     static Boolean exit = false;
     static Scanner scanner = new Scanner(System.in);
+    static String[] rows;
 
-    public static void main(String[] args) {
-        String[] rows = enterRows(scanner);
+    public static void main(String[] args) throws IOException {
+        String inputFile = "";
+        for (int i = 0; i < args.length; i++) {
+            if (args[i].equals("--data")) {
+                inputFile = args[i + 1];
+            }
+
+            rows = importRows(inputFile);
+        }
+
         do {
             menu();
             int action = Integer.parseInt(scanner.nextLine());
@@ -26,9 +38,28 @@ public class Main {
                     System.out.println("Incorrect option! Try again.\n");
             }
         } while (!exit);
+
     }
 
-    public static String[] enterRows(Scanner scanner) {
+    public static String[] importRows(String filename) throws IOException {
+
+        FileReader fileReader = new FileReader(filename);
+
+        BufferedReader bufferedReader = new BufferedReader(fileReader);
+        List<String> lines = new ArrayList<String>();
+        String line = null;
+
+        while ((line = bufferedReader.readLine()) != null)
+        {
+            lines.add(line);
+        }
+
+        bufferedReader.close();
+
+        return lines.toArray(new String[lines.size()]);
+    }
+
+    public static String[] enterRows(Scanner Scanner) {
         System.out.println("Enter the number of rows to be added:");
         String[] rows = new String[scanner.nextInt()];
         scanner.nextLine();
@@ -52,9 +83,9 @@ public class Main {
         }
     }
 
-    public static void printAllRows(String[] rows){
+    public static void printAllRows(String[] rows) {
         System.out.println("=== List of rows ===");
-        for (String row : rows){
+        for (String row : rows) {
             System.out.println(row);
         }
     }
@@ -64,13 +95,13 @@ public class Main {
         exit = true;
     }
 
-    public static void menu(){
+    public static void menu() {
         String menu = String.join("\n",
                 "=== Menu ===",
                 "1. Search for matching rows",
                 "2. Print all rows",
                 "0. Exit"
-                );
+        );
         System.out.println(menu);
 
     }
