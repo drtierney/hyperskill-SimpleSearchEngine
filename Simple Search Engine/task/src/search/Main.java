d05fab20a4452f3bc729ade4e22b9dfe8a332649
David Tierney <d.r.tierney@hotmail.co.uk>
2020-07-05 11:36:30 +0100

    Stage #3 completed
    Stage #3 User menu
    Using switch operator and methods, create a user menu for your search engine.

--- Simple Search Engine/task/src/search/Main.java
+++ Simple Search Engine/task/src/search/Main.java
@@ -1,31 +1,77 @@
 package search;
 
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+    static Boolean exit = false;
+    static Scanner scanner = new Scanner(System.in);
 
+    public static void main(String[] args) {
+        String[] rows = enterRows(scanner);
+        do {
+            menu();
+            int action = Integer.parseInt(scanner.nextLine());
+            switch (action){
+                case 1:
+                    searchRows(scanner, rows);
+                    break;
+                case 2:
+                    printAllRows(rows);
+                    break;
+                case 0:
+                    exit();
+                    break;
+                default:
+                    System.out.println("Incorrect option! Try again.\n");
+            }
+        } while (!exit);
+    }
+
+    public static String[] enterRows(Scanner scanner) {
         System.out.println("Enter the number of rows to be added:");
         String[] rows = new String[scanner.nextInt()];
         scanner.nextLine();
         System.out.println(String.format("Enter %d rows:", rows.length));
-        for (int i = 0; i < rows.length; i++) {
+        int i = 0;
+        while (i < rows.length) {
             String input = scanner.nextLine();
             rows[i] = input;
+            i++;
         }
+        return rows;
+    }
 
-        System.out.println("Enter the number of search queries:");
-        int searchCount = scanner.nextInt();
-        scanner.nextLine();
-        for (int i = 0; i < searchCount; i++) {
-            System.out.println("Enter search query:");
-            String search = scanner.nextLine();
-            for (int j = 0; j < rows.length; j++) {
-                if (rows[j].toUpperCase().contains(search.toUpperCase())){
-                    System.out.println(rows[j].trim());
-                }
+    public static void searchRows(Scanner scanner, String[] rows) {
+        System.out.println("Enter search query:");
+        String search = scanner.nextLine();
+        for (String row : rows) {
+            if (row.toUpperCase().contains(search.toUpperCase())) {
+                System.out.println(row.trim());
             }
         }
+    }
+
+    public static void printAllRows(String[] rows){
+        System.out.println("=== List of rows ===");
+        for (String row : rows){
+            System.out.println(row);
+        }
+    }
+
+    public static void exit() {
+        System.out.println("Bye!");
+        exit = true;
+    }
+
+    public static void menu(){
+        String menu = String.join("\n",
+                "=== Menu ===",
+                "1. Search for matching rows",
+                "2. Print all rows",
+                "0. Exit"
+                );
+        System.out.println(menu);
+
     }
 }
